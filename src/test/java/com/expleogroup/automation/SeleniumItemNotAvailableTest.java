704d73f1542b0d22ae336f45fdd40adc6649fd90
FSullExpleo <60355096+FSullExpleo@users.noreply.github.com>
2022-05-21 17:01:36 +0100

    item not available test

--- src/test/java/com/expleogroup/automation/SeleniumItemNotAvailableTest.java
+++ src/test/java/com/expleogroup/automation/SeleniumItemNotAvailableTest.java
@@ -1,32 +1,42 @@
 package com.expleogroup.automation;
 
-import java.util.ArrayList;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.Assert.fail;
+
+import com.codeborne.selenide.Condition;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.interactions.Actions;
 
-public class SeleniumListTest {
+public class SeleniumItemNotAvailableTest {
 
   @Test
-  public void expleoSeleniumListTest() {
+  public void itemNotAvailableTest() {
 
     System.setProperty("webdriver.chrome.driver", "C:\\Temp\\webdrivers\\chromedriver.exe");
     WebDriver driver = new ChromeDriver();
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     driver.get("https://selenide.org");
 
-    // capture list of elements
-    List<WebElement> elements;
-    elements = driver.findElements(By.cssSelector("div.quicklinks > div > ul > li"));
-
-    Assert.assertTrue("Incorrect number of social media links", elements.size() == 5);
+    // Selenium
+    try {
+      WebElement element = driver.findElement(By.cssSelector("h7"));
+      fail("Element should not exist");
+    } catch (WebDriverException e) {
+      // expected behaviour if item does not exist
+    }
     driver.quit();
+
+    // Selenide
+    //    open("https://selenide.org");
+    //    $("h7").shouldNot(exist);
   }
 }
