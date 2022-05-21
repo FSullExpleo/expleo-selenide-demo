bd999057dca6a16d67c0d9099b430e751350afc9
FSullExpleo <60355096+FSullExpleo@users.noreply.github.com>
2022-05-21 17:00:14 +0100

    item not available test

---
+++ src/test/java/com/expleogroup/automation/SeleniumItemNotAvailableTest.java
@@ -0,0 +1,32 @@
+package com.expleogroup.automation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class SeleniumListTest {
+
+  @Test
+  public void expleoSeleniumListTest() {
+
+    System.setProperty("webdriver.chrome.driver", "C:\\Temp\\webdrivers\\chromedriver.exe");
+    WebDriver driver = new ChromeDriver();
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    driver.get("https://selenide.org");
+
+    // capture list of elements
+    List<WebElement> elements;
+    elements = driver.findElements(By.cssSelector("div.quicklinks > div > ul > li"));
+
+    Assert.assertTrue("Incorrect number of social media links", elements.size() == 5);
+    driver.quit();
+  }
+}
